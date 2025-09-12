40aeec006f0ffdd449f70ef07304bc1dea3eaf39
Hrishabh Gupta <118746244+Hrishabh21@users.noreply.github.com>
2025-09-12 15:22:12 +0530

    Change homepage redirect to index.html

--- box-breathing.js
+++ box-breathing.js
@@ -7,7 +7,7 @@ let phaseTimeout = null;
 let breathingCycle = null;
 
 function goToHomepage() {
-    window.location.href = 'homepage.html';
+    window.location.href = 'index.html';
 }
 
 // Breathing phases
